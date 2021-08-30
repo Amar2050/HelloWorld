b531da3c272ba1599a546860dc8ddff09021565d
Amar2050 <amarboutayeb@gmail.com>
2021-08-30 16:27:13 +0200

    Correction mini exo - Formulaire Création de voiture

--- src/app/cars-list/new-car/new-car.component.ts
+++ src/app/cars-list/new-car/new-car.component.ts
@@ -1,20 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+  import { Component, OnInit } from '@angular/core';
+  import { NgForm } from '@angular/forms';
+  import { Router } from '@angular/router';
+  import { Car } from 'src/app/models/Car';
+  import { DataService } from 'src/app/services/data.service';
 
-@Component({
-  selector: 'app-new-car',
-  templateUrl: './new-car.component.html',
-  styleUrls: ['./new-car.component.css']
-})
-export class NewCarComponent implements OnInit {
+  @Component({
+    selector: 'app-new-car',
+    templateUrl: './new-car.component.html',
+    styleUrls: ['./new-car.component.css']
+  })
+  export class NewCarComponent implements OnInit {
 
-  constructor() { }
+    constructor(private router:Router,private data:DataService) { }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
+    }
+    
+
+    onSubmit(myForm: NgForm){
+      const car = new Car(
+        myForm.value['name'],
+        myForm.value['pays'],
+        myForm.value['coverImage'],
+        myForm.value['power'],
+        myForm.value['perf']
+        );
+      this.data.addCar(car);
+      
+      this.router.navigate(['cars']);
+    }
+
   }
-
-  onSubmit(myForm: NgForm){
-    console.log(myForm.value);
-  }
-
-}
