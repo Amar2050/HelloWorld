9c62b76c6d80cc850dedcb8c890dfb938c1f4255
Amar2050 <amarboutayeb@gmail.com>
2021-08-31 10:22:02 +0200

    Reactive Form - Import du ReactiveFormsModule
    Nécessaire pour le utiliser le reactive form d'Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
 import { NgModule , LOCALE_ID} from '@angular/core';
-
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-
 import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
 registerLocaleData(localeFr, 'fr');
 
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { DataService } from './services/data.service';
+
 
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './header/header.component';
 import { CarsListComponent } from './cars-list/cars-list.component';
 import { DriversListComponent } from './drivers-list/drivers-list.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 import { CarItemComponent } from './cars-list/car-item/car-item.component';
-import { FormsModule } from '@angular/forms';
 import { DriverItemComponent } from './drivers-list/driver-item/driver-item.component';
-import { DataService } from './services/data.service';
 import { FooterComponent } from './footer/footer.component';
 import { NewCarComponent } from './cars-list/new-car/new-car.component';
 
@@ -38,7 +39,8 @@ import { NewCarComponent } from './cars-list/new-car/new-car.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [{ provide: LOCALE_ID, useValue: "fr-FR" },
               DataService
